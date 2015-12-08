2b4b11e69a640ef69a42092946d792e48ad8ca00
pǝɯɐɥoɯ <mo7_abd@hotmail.com>
2015-12-08 12:24:40 -0500

    Revert "Some answers for question 1"
    This reverts commit 96c6d21b33c5b09cb84e297442a9e440274cc379.

--- 2.3/myroot/convert key to pk8 .bat
+++ 2.3/myroot/convert key to pk8 .bat
@@ -2,5 +2,5 @@ openssl pkcs8 -topk8 -in alice.key -out alice.pk8
 REM pwd alice
 openssl pkcs8 -topk8 -in bob.key -out bob.pk8
 REM pwd bobkey
-REM pwd server
 openssl pkcs8 -topk8 -in server.key -out server.pk8
+REM pwd server
