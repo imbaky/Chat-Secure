d2eb1a8df64ba27c3229abe194583f6e97b874a7
pǝɯɐɥoɯ <mo7_abd@hotmail.com>
2015-12-08 19:42:39 -0500

    crt and p8k

--- 2.3/myroot/openssl_commands.bat
+++ 2.3/myroot/openssl_commands.bat
@@ -7,7 +7,7 @@ REM Generate a 4096-bit RSA key in ca.key
 openssl genrsa -aes256 -out ca.key 4096
 REM pwd chat
 REM Generate a self-signed x509v3 certificate valid for 20 years using SHA256 in ca.crt
-openssl req -config openssl.cnf -key ca.key -new -x509 -days 7300 -sha256 -extensions v3_ca -out ca.crt -subj "/C=CA/ST=Montreal/ON=Concordiay"
+openssl req -config openssl.cnf -key ca.key -new -x509 -days 7300 -sha256 -extensions v3_ca -out ca.crt -subj "/C=CA/ST=Montreal/ON=Concordia/CN=MyRoot" 
 
 
 REM Generate leaf certificate
@@ -18,18 +18,29 @@ openssl genrsa -aes256 -out alice.key 2048
 REM pwd alice
 
 REM Generate a Certificate Signing Request (CSR) in alice.csr using SHA256
-openssl req -config openssl.cnf -key alice.key -new -sha256 -out alice.csr  -subj "/C=CA/ST=Montreal/ON=Concordia/CN=Alice"
+openssl req -config openssl.cnf -key alice.key -new -sha256 -out alice.csr  -subj "/C=CA/ST=Montreal/O=Concordia/CN=Alice"
 
+openssl ca -config openssl.cnf -extensions usr_cert -days 365 -notext -md sha256 -in alice.csr -out alice.crt
 
 REM for Bob
 openssl genrsa -aes256 -out bob.key 2048
 REM pwd bob
 REM Generate a Certificate Signing Request (CSR) in bob.csr using SHA256
-openssl req -config openssl.cnf -key bob.key -new -sha256 -out bob.csr -subj "/C=CA/ST=Montreal/ON=Concordia/CN=Bob"
+openssl req -config openssl.cnf -key bob.key -new -sha256 -out bob.csr -subj "/C=CA/ST=Montreal/O=Concordia/CN=Bob"
 
+openssl ca -config openssl.cnf -extensions usr_cert -days 365 -notext -md sha256 -in bob.csr -out bob.crt
 
 REM for Central server
 openssl genrsa -aes256 -out server.key 2048
 REM pwd server
 REM Generate a Certificate Signing Request (CSR) in server.csr using SHA256
-openssl req -config openssl.cnf -key server.key -new -sha256 -out server.csr -subj "/C=CA/ST=Montreal/ON=Concordia/CN=Server"
+openssl req -config openssl.cnf -key server.key -new -sha256 -out server.csr -subj "/C=CA/ST=Montreal/O=Concordia/CN=Server"
+
+openssl ca -config openssl.cnf -extensions server_cert -days 365 -notext -md sha256 -in server.csr -out server.crt
+
+openssl pkcs8 -topk8 -in alice.key -out alice.pk8
+REM pwd alice
+openssl pkcs8 -topk8 -in bob.key -out bob.pk8
+REM pwd bobkey
+openssl pkcs8 -topk8 -in server.key -out server.pk8
+REM pwd server
