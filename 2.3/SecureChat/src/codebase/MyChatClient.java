8196fc499330946d5118f7c5950b477910288da9
ephilippona <ephilippona@gmail.com>
2015-12-08 20:51:25 -0500

    RSA Encrypt + Decrypt methods

--- 2.3/SecureChat/src/codebase/MyChatClient.java
+++ 2.3/SecureChat/src/codebase/MyChatClient.java
@@ -38,6 +38,7 @@ import java.security.cert.*;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Base64;
 import java.security.interfaces.RSAPrivateKey;
 
 /**
@@ -57,6 +58,8 @@ class MyChatClient extends ChatClient {
 	PrivateKey privateKey;
 	PublicKey serverPublicKey;
 	boolean loginWPass=true;
+	Cipher cipher;
+
 	
 	MyChatClient(boolean IsA) { // This is the minimum constructor you must
 								// preserve
@@ -64,6 +67,14 @@ class MyChatClient extends ChatClient {
 		startComm(); // starts the communication
 		File serverCert = new File("../myroot/server.crt");
 		serverPublicKey = loadCertificate(serverCert);
+		
+		try {
+			cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 	/** The current user that is logged in on this client **/
@@ -374,5 +385,47 @@ class MyChatClient extends ChatClient {
 		chatlog = newl;
 		RefreshList();
 
+	}
+	
+	public synchronized String Encrypt(String message, PublicKey pubKey)
+	{
+		 try{
+
+			// initialize Encrypt Mode cipher 
+			cipher.init(Cipher.ENCRYPT_MODE, pubKey);                      
+            //Encrypt
+            byte[] bytesMessage = cipher.doFinal(message.getBytes());
+            
+            //Encode
+            String encryptedMessage = Base64.getEncoder().encodeToString(bytesMessage);
+
+            return encryptedMessage;
+         	
+         }catch(Exception e){
+         	System.err.println("Encrypt: " + e.getMessage());
+         }
+		 return null;
+	}
+	
+	public synchronized String Decrypt(String message, PrivateKey privKey)
+	{
+		 try{
+			 
+			 	//initialize decrypt mode cipher
+				cipher.init(Cipher.DECRYPT_MODE, privKey);                      
+ 	            
+				//Decode
+				byte[] bytesMessage = Base64.getDecoder().decode(message);
+				
+				//Decrypt
+ 	            byte[] plainTextBytes = cipher.doFinal(bytesMessage);
+
+ 	            return new String(plainTextBytes);
+	         	
+	         }catch(Exception e){
+	         	System.err.println("Decrypt: " + e.getMessage());
+	         }
+			 return null;
+	
 	}
 }
